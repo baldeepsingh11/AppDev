74a79c2e0c93b21339fdf090e09098537967385d
Baldeep <chhabrabaldeepsingh@gmail.com>
2020-05-06 19:30:26 +0530

    Intent Service Demo

---
+++ ServiceDemo/app/src/main/java/com/example/servicedemo/MyIntentService.java
@@ -0,0 +1,40 @@
+package com.example.servicedemo;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+public class MyIntentService extends IntentService {
+    final static String CASHBACK_INFO = "cashback_info";
+
+    public MyIntentService() {
+        super("Cashback IntentService");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        String cb_category = intent.getStringExtra("cashback_cat");
+
+        String cbinfo = getCashbackInfo(cb_category);
+        sendCashbackInfoToClient(cbinfo);
+    }
+
+    private String getCashbackInfo(String cbcat) {
+        String cashback;
+        if ("electronics".equals(cbcat)) {
+            cashback = "Upto 20% cashback on electronics";
+        } else if ("fashion".equals(cbcat)) {
+            cashback = "Upto 60% cashbak on all fashion items";
+        } else {
+            cashback = "All other categories except fashion and electronics, flat 30% cashback";
+        }
+        return cashback;
+    }
+
+    private void sendCashbackInfoToClient(String msg) {
+        Intent intent = new Intent();
+        intent.setAction(CASHBACK_INFO);
+        intent.putExtra("cashback", msg);
+        sendBroadcast(intent);
+    }
+}
+
