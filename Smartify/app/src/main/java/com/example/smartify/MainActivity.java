d9203fc8ee7c867f8e7575cd16e2e59b2b686b58
anfal <anfalansari1@gmail.com>
2020-05-21 16:30:22 +0530

    Smartify Begins

---
+++ Smartify/app/src/main/java/com/example/smartify/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.smartify;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Boolean isFirstRun = getSharedPreferences("PREFERENCE", MODE_PRIVATE)
+                .getBoolean("isFirstRun", true);
+        if (isFirstRun) {
+            //show start activity
+            startActivity(new Intent(MainActivity.this, OnBoarding.class));
+            Toast.makeText(MainActivity.this, "First Run", Toast.LENGTH_LONG)
+                    .show();
+        }
+
+
+    }
+}
