b6dc7e85777284138f347e4290736d661dc1e22c
Baldeep <chhabrabaldeepsingh@gmail.com>
2020-05-23 15:26:23 +0530

    added smartify

---
+++ Smartify/app/src/main/java/com/example/smartify/Flip.java
@@ -0,0 +1,55 @@
+package com.example.smartify;
+
+import android.content.res.ColorStateList;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
+
+import android.view.View;
+
+import co.mobiwise.materialintro.shape.Focus;
+import co.mobiwise.materialintro.shape.FocusGravity;
+import co.mobiwise.materialintro.shape.ShapeType;
+import co.mobiwise.materialintro.view.MaterialIntroView;
+
+public class Flip extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flip);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        FloatingActionButton fab = findViewById(R.id.fab);
+        new MaterialIntroView.Builder(this)
+                .enableDotAnimation(true)
+                .enableIcon(true)
+                .setFocusGravity(FocusGravity.CENTER)
+                .setFocusType(Focus.MINIMUM)
+                .setDelayMillis(500)
+                .enableFadeAnimation(true)
+                .performClick(true)
+                .enableDotAnimation(true)
+                .setInfoText("Click here to enable DND when phone is flipped")
+                .setShape(ShapeType.CIRCLE)
+                .setTarget(fab)
+                .setUsageId("intro_fab") //THIS SHOULD BE UNIQUE ID
+                .setMaskColor(ContextCompat.getColor(Flip.this,R.color.whiteTransparent))
+                .show();
+        fab.setImageResource(R.drawable.ic_do_not_disturb_on_black_24dp);
+        fab.setBackgroundTintList(ColorStateList.valueOf(ContextCompat.getColor(Flip.this, R.color.colorPrimary)));
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
