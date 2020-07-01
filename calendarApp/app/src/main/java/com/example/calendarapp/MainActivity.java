5f24c86a255f6c07c2319dc9908170ab6e59c745
anfal <anfalansari1@gmail.com>
2020-07-01 19:47:19 +0530

    calender app

---
+++ calendarApp/app/src/main/java/com/example/calendarapp/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.calendarapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.RectF;
+import android.os.Bundle;
+
+import com.alamkanak.weekview.MonthLoader;
+import com.alamkanak.weekview.WeekView;
+import com.alamkanak.weekview.WeekViewEvent;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    WeekView mWeekView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mWeekView = findViewById(R.id.weekView);
+
+        mWeekView.setOnEventClickListener(new WeekView.EventClickListener() {
+            @Override
+            public void onEventClick(WeekViewEvent event, RectF eventRect) {
+
+            }
+        });
+
+        mWeekView.setMonthChangeListener(new MonthLoader.MonthChangeListener() {
+            @Override
+            public List<WeekViewEvent> onMonthChange(int newYear, int newMonth) {
+                List<WeekViewEvent> events = new ArrayList<WeekViewEvent>();
+
+                Calendar calendar = Calendar.getInstance();
+                Calendar calendar1 = Calendar.getInstance();
+                calendar1.add(Calendar.HOUR, 1);
+                WeekViewEvent someEvent = new WeekViewEvent(1,"someEvent",calendar,calendar1);
+                events.add(someEvent);
+                return events;
+            }
+        });
+    }
+}
