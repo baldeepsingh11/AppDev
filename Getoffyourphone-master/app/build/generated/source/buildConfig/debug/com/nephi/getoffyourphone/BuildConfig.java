9a9e1f698ac48b2b75d6ed05f09678372b49219f
anfal <anfalansari1@gmail.com>
2020-07-09 19:11:31 +0530

    material design sample added

---
+++ Getoffyourphone-master/app/build/generated/source/buildConfig/debug/com/nephi/getoffyourphone/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.nephi.getoffyourphone;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.nephi.getoffyourphone";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 45;
+  public static final String VERSION_NAME = "7.0r";
+}
